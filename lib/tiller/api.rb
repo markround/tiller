d06c6d4a594ea1f952d6779ac7fdd2b4884925bd
Mark Round <mark.round@bskyb.com>
2014-10-21 09:40:12 +0100

    Exception wrapping and display startup message regardless of verbosity level

--- lib/tiller/api.rb
+++ lib/tiller/api.rb
@@ -15,53 +15,60 @@ API_PORT=6275
 # after it has generated templates and forked a child process.
 
 def tiller_api(tiller_api_hash)
-  api_port = tiller_api_hash['config']['api_port'] ?
-      tiller_api_hash['config']['api_port'] : API_PORT
 
-  puts "Tiller API starting on port #{api_port}" if tiller_api_hash['config'][:verbose]
+  begin
+    api_port = tiller_api_hash['config']['api_port'] ?
+        tiller_api_hash['config']['api_port'] : API_PORT
 
-  server = TCPServer.new(api_port)
+    puts "Tiller API starting on port #{api_port}"
 
-  loop do
-    socket = server.accept
-    request = socket.gets
-    (method, uri, http_version) = request.split
+    server = TCPServer.new(api_port)
 
-    if uri =~ /^\/v([0-9]+)\//
-      api_version = uri.split('/')[1]
+    loop do
+      socket = server.accept
+      request = socket.gets
+      (method, uri, http_version) = request.split
+
+      if uri =~ /^\/v([0-9]+)\//
+        api_version = uri.split('/')[1]
+      end
+
+      # Defaults
+      response = handle_404
+
+      # Routing
+      case method
+        when 'GET'
+          case uri
+            when '/ping'
+              response = handle_ping
+            when /^\/v([0-9]+)\/config/
+              response = handle_config(api_version, tiller_api_hash)
+            when /^\/v([0-9]+)\/globals/
+              response = handle_globals(api_version, tiller_api_hash)
+            when /^\/v([0-9]+)\/templates/
+              response = handle_templates(api_version, tiller_api_hash)
+            when /^\/v([0-9]+)\/template\//
+              template = uri.split('/')[3]
+              response = handle_template(api_version, tiller_api_hash, template)
+          end
+      end
+
+      # Response
+      socket.print "HTTP/1.1 #{response[:status]}\r\n" +
+                       "Content-Type: application/json\r\n" +
+                       "Server: Tiller #{VERSION} / API v#{API_VERSION}\r\n"
+                       "Content-Length: #{response[:content].bytesize}\r\n" +
+                       "Connection: close\r\n"
+      socket.print "\r\n"
+      socket.print response[:content]
+      socket.close
     end
 
-    # Defaults
-    response = handle_404
-
-    # Routing
-    case method
-      when 'GET'
-        case uri
-          when '/ping'
-            response = handle_ping
-          when /^\/v([0-9]+)\/config/
-            response = handle_config(api_version, tiller_api_hash)
-          when /^\/v([0-9]+)\/globals/
-            response = handle_globals(api_version, tiller_api_hash)
-          when /^\/v([0-9]+)\/templates/
-            response = handle_templates(api_version, tiller_api_hash)
-          when /^\/v([0-9]+)\/template\//
-            template = uri.split('/')[3]
-            response = handle_template(api_version, tiller_api_hash, template)
-        end
-    end
-
-    # Response
-    socket.print "HTTP/1.1 #{response[:status]}\r\n" +
-                     "Content-Type: application/json\r\n" +
-                     "Server: Tiller #{VERSION} / API v#{API_VERSION}\r\n"
-                     "Content-Length: #{response[:content].bytesize}\r\n" +
-                     "Connection: close\r\n"
-    socket.print "\r\n"
-    socket.print response[:content]
-    socket.close
+  rescue Exception => e
+    puts "Error : Exception in Tiller API thread : #{e.class.name}\n#{e}"
   end
+
 end
 
 
