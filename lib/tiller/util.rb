8f43b03b74c11b99248fbdbf0ee24984dcc5ba47
Mark Round <mark.round@bskyb.com>
2015-03-17 14:43:38 +0000

    Added array form of exec

--- lib/tiller/util.rb
+++ lib/tiller/util.rb
@@ -13,11 +13,30 @@ def warn_merge(key, old, new, type, source)
 end
 
 # Pass signals on to child process
-def signal(sig, pid, config)
-  puts "Caught signal #{sig}, passing to PID #{pid}" if config[:verbose]
+def signal(sig, pid, options={})
+  puts "Caught signal #{sig}, passing to PID #{pid}" if options[:verbose]
   begin
     Process.kill(sig, pid)
   rescue Errno::ESRCH
     false
   end
+end
+
+# Launch the replacement process.
+def launch(cmd, options={})
+  # If an array, then we use a different form of spawn() which
+  # avoids a subshell. See https://github.com/markround/tiller/issues/8
+  if cmd.is_a?(Array)
+    final='cmd[0]'
+    # Build up the list of arguments when using the array form of spawn()
+    if cmd.size > 1
+      (1..cmd.size-1).each {|c| final="#{final} , cmd[#{c}]" }
+    end
+    puts 'Using array exec' if options[:verbose]
+    pid=eval "spawn(#{final})"
+  else
+    pid=spawn(cmd)
+  end
+
+  pid
 end
