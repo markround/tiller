959bcaa63b1d89f0c5ce3ce0ac170b719bc0259d
Mark Round <github@markround.com>
2017-02-06 15:46:00 +0000

    Deprecate globals API endpoint

--- lib/tiller/api/handlers/globals.rb
+++ lib/tiller/api/handlers/globals.rb
@@ -2,10 +2,11 @@ require 'tiller/json'
 require 'tiller/api/handlers/404'
 
 def handle_globals(api_version, tiller_api_hash)
+  warning_json = { deprecation_warning: 'The v1 Tiller API is deprecated. Expect this endpoint to be removed in future versions.' }
   case api_version
     when 'v1'
       {
-          :content => dump_json(tiller_api_hash['global_values']),
+          :content => dump_json(tiller_api_hash['global_values'].merge(warning_json)),
           :status => '200 OK'
       }
     else
