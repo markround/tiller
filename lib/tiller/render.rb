3e2ac01726fdb7bd606a32c54fdc1e1307b144c0
em <eugen.mayer@kontextwork.de>
2023-07-09 12:36:02 +0200

    Fix deprecated ERB usage

--- lib/tiller/render.rb
+++ lib/tiller/render.rb
@@ -7,7 +7,7 @@ module Tiller
       b = binding
       ns =  options[:namespace]
       ns.each { |k, v| b.local_variable_set(k, v) }
-      return ERB.new(template, nil, '-').result(ns.instance_eval { b })
+      return ERB.new(template, nil, trim_mode: '-').result(ns.instance_eval { b })
     end
 
     ns = OpenStruct.new(Tiller::tiller)
@@ -15,14 +15,14 @@ module Tiller
     # This is used for rendering content in dynamic configuration files
     if options.has_key?(:direct_render)
       content = template
-      return ERB.new(content, nil, '-').result(ns.instance_eval { binding })
+      return ERB.new(content, nil, trim_mode: '-').result(ns.instance_eval { binding })
     end
 
     if Tiller::templates.key?(template)
       content = Tiller::templates[template]
-      ERB.new(content, nil, '-').result(ns.instance_eval { binding })
+      ERB.new(content, nil, trim_mode: '-').result(ns.instance_eval { binding })
     else
-      Tiller::log.warn("Warning : Requested render of non-existant template #{template}")
+      Tiller::log.warn("Warning : Requested render of non-existent template #{template}")
       ""
     end
   end
